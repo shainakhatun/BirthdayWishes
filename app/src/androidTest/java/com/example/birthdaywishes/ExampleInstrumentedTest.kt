6face6ada871045a510f06c6903d8da32e930ed1
Shaina <116898401+shainakhatun@users.noreply.github.com>
2023-03-08 12:57:33 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/birthdaywishes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/birthdaywishes/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.birthdaywishes", appContext.packageName)
+        assertEquals("com.example.birthday wishes", appContext.packageName)
     }
 }
